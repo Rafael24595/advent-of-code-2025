00af3ad926f263d29c7149c15fc265808a5b66d8
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-12-01 17:43:54 +0100

    [FEATURE] Initial commit.

---
+++ src/exercise_01.zig
@@ -0,0 +1,90 @@
+const std = @import("std");
+
+pub fn part_1() !void {
+    const alloc = std.heap.page_allocator;
+
+    const steps = try parse_input(alloc);
+    defer alloc.free(steps);
+
+    const positions: u8 = 100;
+    var cursor: u8 = 50;
+
+    var loops: u16 = 0;
+    var count: u16 = 0;
+    for (steps) |step| {
+        const loop = rotations(0, positions, cursor, step);
+
+        cursor = move(positions, cursor, step);
+        if (cursor == 0) {
+            count += 1;
+        }
+
+        loops += loop;
+
+        var fix_step: i64 = step;
+        var direction: []const u8 = "left";
+        if (step < 0) {
+            direction = "right";
+            fix_step = step * -1;
+        }
+
+        std.debug.print("Move {d} clicks to {s}: {d} | Loops: {d}\n", .{ fix_step, direction, cursor, loops });
+    }
+
+    std.debug.print("\nTotal zeroes: {d}\n", .{count});
+    std.debug.print("Loops: {d}\n", .{loops});
+}
+
+fn parse_input(allocator: std.mem.Allocator) ![]i64 {
+    var file = try std.fs.cwd().openFile("src/source/exercise_01_00.txt", .{});
+    defer file.close();
+
+    var buffer: [1024]u8 = undefined;
+
+    var file_reader = file.reader(&buffer);
+    const reader = &file_reader.interface;
+
+    var list = try std.ArrayList(i64).initCapacity(allocator, 0);
+
+    while (reader.takeDelimiterExclusive('\n')) |line| {
+        var mult: i8 = 1;
+        if (line.len > 0 and line[0] == 'L') {
+            mult = -1;
+        }
+
+        const number_slice = std.mem.trim(u8, line[1..], " \t\r");
+        const value = try std.fmt.parseInt(i64, number_slice, 10);
+        try list.append(allocator, value * mult);
+    } else |_| {}
+
+    return list.items;
+}
+
+fn move(positions: u8, cursor: u8, movement: i64) u8 {
+    const new_cursor = @mod(cursor + movement, positions);
+    return @intCast(new_cursor);
+}
+
+fn rotations(target: u8, positions: u8, cursor: u8, movement: i64) u8 {
+    const i_target: i8 = @intCast(target);
+    const i_cursor: i8 = @intCast(cursor);
+    const i_positions: i8 = @intCast(positions);
+
+    if (movement > 0) {
+        const after = @divFloor(i_cursor - i_target + movement, i_positions);
+        const before = @divFloor(i_cursor - i_target, i_positions);
+        return @intCast(@abs(after - before));
+    } 
+    
+    if (movement < 0) {
+        const after = @divFloor(i_target - i_cursor, i_positions);
+        const before = @divFloor(i_target - (i_cursor + movement), i_positions);
+        return @intCast(@abs(after - before));
+    }
+
+    if (i_cursor == i_target) {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const exercise_01 = @import("exercise_01.zig");
+
+pub fn main() !void {
+    try exercise_01.part_1();
+}
